32feca099a8dee9bbe41ac732de12cd634d874ab
Blaze It <realblazeit@gmail.com>
2017-02-27 20:54:50 -0500

    Base

--- DEA/DEA/Program.cs
+++ DEA/DEA/Program.cs
@@ -8,9 +8,38 @@ namespace DEA
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        // Convert our sync main to an async main.
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
 
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+            // Define the DiscordSocketClient
+            client = new DiscordSocketClient();
+
+            var token = "token here";
+
+            // Login and connect to Discord.
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.ConnectAsync();
+
+            var map = new DependencyMap();
+            map.Add(client);
+
+            handler = new CommandHandler();
+            await handler.Install(map);
+
+            // Block this program until it is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
