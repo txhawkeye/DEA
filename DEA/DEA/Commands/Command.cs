78f9e515ec40bdf0b5dab23d61a2e172076a45be
Apple is Void <1piedude11@gmail.com>
2017-02-27 18:07:12 -0700

    Initial Commit

---
+++ DEA/DEA/Commands/Command.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEA
+{
+    interface Command
+    {
+        enum Perms { RANK1, RANK2, RANK3, RANK4, MODERATOR, ADMINISTRATOR, BOT_OWNER };
+        static bool enabled
+        {
+            get;
+        }
+        static bool guildOnly
+        {
+            get;
+        }
+        static string[] aliases
+        {
+            get;
+        }
+        static string[] userLevel
+        {
+            get;
+        }
+        static string[] botPerms
+        {
+            get;
+        }
+
+        static string help();
+    }
+}
