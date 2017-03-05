e3c1b701c5c9f45a0ec014a8c82c6852ec9c1dcc
Blaze It <realblazeit@gmail.com>
2017-03-05 16:01:04 -0500

    Fixed config file

--- Config.cs
+++ Config.cs
@@ -1,17 +1,15 @@
-﻿using System;
-
-public static class Config
+﻿public static class Config
 {
     public static readonly string TOKEN = "bot token here";
 
     public static readonly int MIN_CHAR_LENGTH = 7, LEADERBOARD_CAP = 20, LINE_COOLDOWN = 25000, WHORE_COOLDOWN = 7200000, JUMP_COOLDOWN = 14400000,
-    
+
     STEAL_COOLDOWN = 21600000, ROB_COOLDOWN = 28800000;
 
     public static readonly float RANK1 = 500, RANK2 = 2500, RANK3 = 5000, RANK4 = 10000, LINE_COST = 250, POUND_COST = 1000,
 
-    KILO_COST = 2500, POUND_MULTIPLIER = 2, KILO_MULTIPLIER = 4, RESET_REWARD = 10000, HIGHEST_WHORE = 100, 
-        
+    KILO_COST = 2500, POUND_MULTIPLIER = 2, KILO_MULTIPLIER = 4, RESET_REWARD = 10000, HIGHEST_WHORE = 100,
+
     HIGHEST_JUMP = 250, HIGHEST_STEAL = 500, MAX_RESOURCES = 1000, MIN_RESOURCES, TEMP_MULTIPLIER_RATE = 0.1f, MAX_TEMP_MULTIPLIER = 10;
 
     public static readonly string[] BANKS = { "Bank of America", "Wells Fargo Bank", "JPMorgan Chase Bank", "Capital One Bank",
