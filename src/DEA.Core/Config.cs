7a5f3587b4280355aba4dc44aac7736b330f66d3
RealBlazeIt <RealBlazeIt@users.noreply.github.com>
2017-03-05 09:42:36 -0500

    Update Config.cs

--- src/DEA.Core/Config.cs
+++ src/DEA.Core/Config.cs
@@ -2,7 +2,7 @@
 
 public static class Config
 {
-    public static readonly string TOKEN = "MjgwMjAxNzUwMjQ0MzYwMTk0.C5keHw.Nj6fcpH5igw15CmcEcelaz1V5d4";
+    public static readonly string TOKEN = "bot token here";
 
     public static readonly int MIN_CHAR_LENGTH = 7, LEADERBOARD_CAP = 20, LINE_COOLDOWN = 25000, WHORE_COOLDOWN = 7200000, JUMP_COOLDOWN = 14400000,
     
