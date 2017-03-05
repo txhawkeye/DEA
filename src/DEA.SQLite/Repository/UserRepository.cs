1eea8da7cb73ea5cfb6bed8c6290316396849897
Apple is Void <1piedude11@gmail.com>
2017-03-04 19:43:40 -0700

    Added Whore and Jump

--- src/DEA.SQLite/Repository/UserRepository.cs
+++ src/DEA.SQLite/Repository/UserRepository.cs
@@ -51,6 +51,36 @@ namespace DEA.SQLite.Repository
             return DateTime.Parse(user.LastMessage);
         }
 
+        public async Task<DateTime> GetLastWhore(ulong userId)
+        {
+            var user = await FetchUser(userId);
+            return DateTime.Parse(user.LastWhore);
+        }
+
+        public async Task<DateTime> GetLastJump(ulong userId)
+        {
+            var user = await FetchUser(userId);
+            return DateTime.Parse(user.LastJump);
+        }
+
+        public async Task<DateTime> GetLastSteal(ulong userId)
+        {
+            var user = await FetchUser(userId);
+            return DateTime.Parse(user.LastSteal);
+        }
+
+        public async Task<DateTime> GetLastRob(ulong userId)
+        {
+            var user = await FetchUser(userId);
+            return DateTime.Parse(user.LastRob);
+        }
+
+        public async Task<DateTime> GetLastReset(ulong userId)
+        {
+            var user = await FetchUser(userId);
+            return DateTime.Parse(user.LastReset);
+        }
+
         public async Task SetTemporaryMultiplier(ulong userId, float tempMultiplier)
         {
             var user = await FetchUser(userId);
@@ -76,13 +106,6 @@ namespace DEA.SQLite.Repository
         {
             var user = await FetchUser(userId);
             user.LastMessage = lastMessage.ToString();
-            await UpdateAsync(user);
-        }
-
-        public async Task SetInvestmentMultiplier(ulong userId, float investmentMultiplier)
-        {
-            var user = await FetchUser(userId);
-            user.InvestmentMultiplier = investmentMultiplier;
             await UpdateAsync(user);
         }
 
